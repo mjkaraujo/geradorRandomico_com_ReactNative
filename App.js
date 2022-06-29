f10c6100a6968812640803dbcd72cb4c925f64f4
Mauricio Araujo <mjk_araujo@hotmail.com>
2022-06-29 17:03:37 -0300

    gerador de números de 0 a 100 em React Native

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+
+  const [numero, setNumero] = useState(0)
+
+  function handleNumero() {
+
+    const novoNumero = Math.floor(Math.random() * 100)
+
+    setNumero(novoNumero)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.img} source={require('./assets/logoBranco.png')}></Image>
+      <Text style={styles.numero}>{numero}</Text>
+      <TouchableOpacity onPress={handleNumero} style={styles.botao}>
+        <Text style={styles.textBotao}>Gerar número</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  numero: {
+    color: 'yellow',
+    fontSize: 150,
+    marginTop: 30,
+  },
+  botao: {
+    backgroundColor: 'yellow',
+    paddingVertical: 20,
+    paddingHorizontal: 50,
+    borderRadius: 15,
+    margin: 70,
+  },
+  textBotao: {
+    color: 'blue',
+    fontSize: 18,
+  },
+  img: {
+    resizeMode: 'contain',
+    alignSelf: 'flex-start',
+    width: 150,
+    height: 150,
+    marginTop: -100,
+    marginLeft: 5,
   },
 });
